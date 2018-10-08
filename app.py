8fa363de6fff88f6d80a4bd6dbc2fc141e5a6ed8
Yuval Adam <_@yuv.al>
2018-10-08 13:43:21 +0300

    Support multiple IPs in header

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 def get_ip(request):
     if 'PROD' in environ:
-        return request.headers.get('X-Forwarded-For')
+        # Heroku routing layers masks the original IP, fetch it from the custom header
+        # And make sure we just take the first IP in case other proxies were in the way
+        ips = request.headers.get('X-Forwarded-For')
+        return ips.split(',')[0]
     else:
         return request.remote_addr
 
