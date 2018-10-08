b4600e01f4bd73981b16da5ce9788f8f32b3c5fa
Yuval Adam <_@yuv.al>
2018-10-08 13:33:22 +0300

    Return forwarded IP on prod

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, request
+from os import environ
 
 app = Flask(__name__)
 
+def get_ip(request):
+    if 'PROD' in environ:
+        return request.headers.get('X-Forwarded-For')
+    else:
+        return request.remote_addr
+
 @app.route('/')
 def index():
-    return request.remote_addr
+    return get_ip(request)
