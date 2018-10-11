7c5cc0d1fe9cb8ac44848827fdcf78691642c98f
Yuval Adam <_@yuv.al>
2018-10-12 00:08:27 +0200

    Basic whois subprocess with parsing

---
+++ whois.py
@@ -0,0 +1,13 @@
+from subprocess import run
+
+def extract_field(field, response):
+    for line in response:
+        if line.startswith(field):
+            return line.split(' ')[-1]
+    
+def whois(ip):
+    cp = run(['whois', ip], capture_output=True)
+    lines = cp.stdout.decode().split('\n')
+    fields = ['route', 'origin']
+    res = {f: extract_field(f, lines) for f in fields}
+    return res
